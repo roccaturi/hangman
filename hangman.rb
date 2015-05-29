ae4bf4b9bafdcc636d4af0831cdbd71f6e34fb1f
roccaturi <brian.burnley@gmail.com>
2015-05-28 20:56:21 -0400

    cleanup

--- hangman.rb
+++ hangman.rb
@@ -6,14 +6,18 @@ require 'gon-sinatra'
 
 Sinatra::register Gon::Sinatra
 
+############
+## METHODS #
+############
+
 def prep_dictionary(filename='enable.txt')
-  # Assumes filename is space-delimited text file.  Returns filename's contents as arr.
+  # Assumes filename is space-delimited text file. Returns filename's contents as arr.
   f = open(filename)
   return f.read.split(" ")
 end
 
 def select_word(dictionary, difficulty)
-  # Assumes dictionary is arr, difficulty is int.  Returns random item from dictionary of length equal to difficulty.
+  # Assumes dictionary is arr, difficulty is int. Returns random item from dictionary of length equal to difficulty.
   index = rand(dictionary.length)
   if dictionary[index].length == difficulty
     return dictionary[index]
@@ -23,7 +27,7 @@ def select_word(dictionary, difficulty)
 end
 
 def select_crimes(dictionary, difficulty)
-  # Assumes dictionary is arr, difficulty is int.  Returns arr of n unique random items from dictionary, where n equals difficulty.
+  # Assumes dictionary is arr, difficulty is int. Returns arr of n unique random items from dictionary, where n equals difficulty.
   crimes = []
   difficulty.times do
     found = false
@@ -39,12 +43,11 @@ def select_crimes(dictionary, difficulty)
 end
 
 def select_nickname(dictionary1, dictionary2)
-  # Assumes dictionary1 and dictionary2 are arr.  Returns a str that concatonates a random item from dictionary1 with a random item from dictionary2.
+  # Assumes dictionary1 and dictionary2 are arr. Returns a str that concatenates a random item from dictionary1 with a random item from dictionary2.
   index = rand(dictionary1.length)
   nickname = dictionary1[index]
   index = rand(dictionary2.length)
-  nickname += " " + dictionary2[index]
-  return nickname
+  return nickname += " " + dictionary2[index]
 end
 
 def prep_game()
@@ -65,35 +68,33 @@ def prep_game()
 end
 
 def check_guess(guess, word)
-  # Assumes guess and word are str.  Returns an arr of indices at which word equals guess, or nil if no matches exist.
+  # Assumes guess and word are str. Returns an arr of indices at which word equals guess, or nil if no match exists.
   indices = []
   word = word.split('')
   word.each_with_index do |x, idx|
-    if x == guess
-      indices << idx
-    end
+    indices << idx if x == guess
   end
   indices.empty? ? nil : indices
 end
 
 def reveal_letters(current_str, guess, indices)
-  # Assumes current_str is str, guess is str, and indices is arr.  Returns current_str with guess subsituted for each index in indices.  Toggles win state if no unknown characters in current_str remain.
+  # Assumes current_str is str, guess is str, and indices is arr. Returns current_str with guess subsituted for each index in indices. Toggles win state if zero unknown chars remain in current_str.
   current_str = current_str.split('')
   indices.each {|x| current_str[x] = guess}
   @@win = true if !current_str.include?('_')
   return current_str.join
 end
 
+###########
+## ROUTES #
+###########
+
 get '/' do
   erb :landing
 end
 
 post '/' do
-  if params["value"] == "What's going on here?"
-    rediret '/about'
-  else
-    redirect '/setup'
-  end
+  redirect '/setup'
 end
 
 get '/about' do
@@ -116,15 +117,15 @@ post '/setup' do
   @@name = params['name']
   @@difficulty = params['difficulty'].to_i
   @@difficulty = 20 if @@difficulty > 20
-  @@difficulty = 3 if @@difficulty < 3
+  @@difficulty = 4 if @@difficulty < 4
   prep_game()
   redirect '/game'
 end
 
 get '/game' do
   gon.missed_appeals = @@missed_appeals
-  erb :game, :layout => false, :locals => {:difficulty => @@difficulty, :word => @@word, :current_str => @@current_str, :wrong_letters => @@wrong_letters, :missed_appeals => @@missed_appeals, :cheat => @@cheat, :crimes => @@crimes, :nickname => @@nickname, :name => @@name}
-  if params["guess"] != nil
+  erb :game, :layout => false, :locals => {:word => @@word, :current_str => @@current_str, :wrong_letters => @@wrong_letters, :missed_appeals => @@missed_appeals, :cheat => @@cheat, :name => @@name, :nickname => @@nickname, :crimes => @@crimes}
+  if params['guess'] != '' && params['guess'] != nil
     guess = params['guess'].capitalize
     @@cheat = true if params['cheat'] == 'true'
     guess_locations = check_guess(guess, @@word)
@@ -140,7 +141,7 @@ get '/game' do
     end
   end
   gon.missed_appeals = @@missed_appeals
-  erb :game, :layout => false, :locals => {:difficulty => @@difficulty, :word => @@word, :current_str => @@current_str, :wrong_letters => @@wrong_letters, :missed_appeals => @@missed_appeals, :cheat => @@cheat, :crimes => @@crimes, :nickname => @@nickname, :name => @@name}
+  erb :game, :layout => false, :locals => {:word => @@word, :current_str => @@current_str, :wrong_letters => @@wrong_letters, :missed_appeals => @@missed_appeals, :cheat => @@cheat, :name => @@name, :nickname => @@nickname, :crimes => @@crimes}
 end
 
 post '/game' do
@@ -148,7 +149,7 @@ post '/game' do
 end
 
 get '/pardoned' do
-  erb :pardoned, :locals => {:guess_counter => @@guess_counter, :missed_appeals => @@missed_appeals, :name => @@name, :nickname => @@nickname, :word => @@word}
+  erb :pardoned, :locals => {:name => @@name, :nickname => @@nickname, :word => @@word, :guess_counter => @@guess_counter, :missed_appeals => @@missed_appeals}
 end
 
 post '/pardoned' do
@@ -156,11 +157,7 @@ post '/pardoned' do
 end
 
 get '/executed' do
-  erb :executed, :locals => {:current_str => @@current_str, :word => @@word, :difficulty => @@difficulty, :name => @@name, :nickname => @@nickname}
-end
-
-get '/executed*' do
-  redirect '/about'
+  erb :executed, :locals => {:name => @@name, :nickname => @@nickname, :word => @@word}
 end
 
 post '/executed' do
